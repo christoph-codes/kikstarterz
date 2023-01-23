e816d2ef72e5e79f88e039e2d090ebbaca6c2ae7
Christopher Jones <tkcwebdev@gmail.com>
2023-01-23 15:22:26 -0800

    1: Create reusable hero component for content pages

---
+++ web/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import { Flex, Image } from "@chakra-ui/react";
+
+const Footer = () => {
+	return (
+		<Flex
+			justifyContent="center"
+			bgColor="brand.black.default"
+			paddingY="8px"
+		>
+			<Image width="24px" src="/favicon@2x.png" alt="Kikstarterz Icon" />
+		</Flex>
+	);
+};
+
+export default Footer;
