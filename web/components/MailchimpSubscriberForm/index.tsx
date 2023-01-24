dc30a6ec2ba257e4fa647ae293be7faa0cd7e837
Christopher Jones <tkcwebdev@gmail.com>
2023-01-24 10:15:45 -0800

    1: Install mailchimp for waitlist subscription

---
+++ web/components/MailchimpSubscriberForm/index.tsx
@@ -0,0 +1,60 @@
+import Mailchimp from "react-mailchimp-subscribe";
+import Form from "../Form";
+import Title from "../Title";
+
+const MailchimpSubscribeForm = () => {
+	return (
+		<>
+			<Mailchimp
+				url={`https://kikstarterz.us10.list-manage.com/subscribe/post?u=${process.env.NEXT_PUBLIC_MAILCHIMP_U}&amp;id=${process.env.NEXT_PUBLIC_MAILCHIMP_ID}&amp;f_id=0038c9e5f0`}
+				render={({ subscribe, status, message }: any) => {
+					return (
+						<>
+							{status === "success" ? (
+								message && (
+									<Title h2 textAlign="center">
+										{message}
+									</Title>
+								)
+							) : (
+								<Form
+									formName="waitlist-subscribe"
+									submitButton={{ children: "Join Waitlist" }}
+									onSubmit={subscribe}
+									inputs={[
+										{
+											name: "FNAME",
+											placeholder: "Johnathan",
+											type: "text",
+											required: true,
+											label: "First Name", //@ts-ignore
+											validation: ["REQUIRED"],
+										},
+										{
+											name: "LNAME",
+											placeholder: "Jones",
+											type: "text",
+											required: true,
+											label: "Last Name", //@ts-ignore
+											validation: ["REQUIRED"],
+										},
+										{
+											name: "EMAIL",
+											placeholder: "john@jones.com",
+											type: "email",
+											required: true,
+											label: "Email", //@ts-ignore
+											validation: ["REQUIRED"],
+										},
+									]}
+								/>
+							)}
+						</>
+					);
+				}}
+			/>
+		</>
+	);
+};
+
+export default MailchimpSubscribeForm;
