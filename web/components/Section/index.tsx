df748a1f567ab530477e663c79ffe3f0aa3c9d81
Christopher Jones <tkcwebdev@gmail.com>
2023-01-24 14:40:26 -0800

    main: Forward reference to section component

--- web/components/Section/index.tsx
+++ web/components/Section/index.tsx
@@ -1,10 +1,10 @@
 import { Box, BoxProps, Text } from "@chakra-ui/react";
-import { FC, ReactNode } from "react";
+import { forwardRef, LegacyRef, ReactNode, Ref } from "react";
 import Container from "@/components/Container";
 import Title from "@/components/Title";
 import styles from "./Section.module.scss";
 
-export type ISectionProps = {
+export interface ISectionProps extends BoxProps {
 	children?: ReactNode;
 	className?: string;
 	containerClass?: string;
@@ -13,54 +13,61 @@ export type ISectionProps = {
 	bgImg?: string;
 	title?: string;
 	description?: string;
-};
+}
 
-const Section: FC<ISectionProps & BoxProps> = ({
-	children,
-	className,
-	containerClass,
-	hideContainer,
-	bgColor,
-	bgImg,
-	title,
-	description,
-	...rest
-}) => {
-	const content = (
-		<>
-			{title && (
-				<Title
-					h2
-					className={styles.Section__title}
-					textAlign="center"
-					marginBottom={16}
-				>
-					{title}
-				</Title>
-			)}
-			{description && (
-				<Text className={styles.Section__description}>
-					{description}
-				</Text>
-			)}
-			{children}
-		</>
-	);
-	return (
-		<Box
-			as="section"
-			bgColor={bgColor || ""}
-			className={`${styles.Section} ${className}`}
-			paddingX={{ base: 4, md: 0 }}
-			backgroundImage={bgImg && `url(${bgImg})`}
-			{...rest}
-		>
-			{!hideContainer ? (
-				<Container className={containerClass}>{content}</Container>
-			) : (
-				content
-			)}
-		</Box>
-	);
-};
+const Section = forwardRef(
+	(
+		{
+			children,
+			className,
+			containerClass,
+			hideContainer,
+			bgColor,
+			bgImg,
+			title,
+			description,
+			...rest
+		}: ISectionProps,
+		ref: LegacyRef<HTMLDivElement>,
+	) => {
+		const content = (
+			<>
+				{title && (
+					<Title
+						h2
+						className={styles.Section__title}
+						textAlign="center"
+						marginBottom={16}
+					>
+						{title}
+					</Title>
+				)}
+				{description && (
+					<Text className={styles.Section__description}>
+						{description}
+					</Text>
+				)}
+				{children}
+			</>
+		);
+		return (
+			<Box
+				as="section"
+				bgColor={bgColor || ""}
+				className={`${styles.Section} ${className}`}
+				paddingX={{ base: 4, md: 0 }}
+				backgroundImage={bgImg && `url(${bgImg})`}
+				ref={ref}
+				{...rest}
+			>
+				{!hideContainer ? (
+					<Container className={containerClass}>{content}</Container>
+				) : (
+					content
+				)}
+			</Box>
+		);
+	},
+);
+Section.displayName = "Section";
 export default Section;
