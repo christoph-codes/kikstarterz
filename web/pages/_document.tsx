03997be37890afb2a7c5670a0ea77cd0032a78ba
christoph-codes <tkcwebdev@gmail.com>
2023-01-25 00:11:12 -0800

    3: Add GTM & GA. Resolves issue #3

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PN7B9CL"
+					height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+					}}
+				/>
+			</body>
+		</Html>
+	);
 }
