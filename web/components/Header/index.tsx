8f553440af2cec476546d902f9d788cea0acb52c
Christopher Jones <tkcwebdev@gmail.com>
2023-01-25 14:48:42 -0800

    7: Setup header to prepare for navigation and add white paper link. Resolves issue #7

--- web/components/Header/index.tsx
+++ web/components/Header/index.tsx
@@ -1,17 +1,22 @@
 import { Flex, Image } from "@chakra-ui/react";
+import NavLink from "../NavLink";
 
 const Header = () => {
 	return (
 		<Flex
-			justifyContent="center"
+			justifyContent="space-between"
 			bgColor="brand.black.default"
 			paddingY="8px"
+			alignItems="center"
+			borderBottom="solid 1px"
+			borderBottomColor="brand.black.hover"
 		>
 			<Image
 				src="/kikstarterz_hori_logo_light.svg"
 				alt="Kikstarterz Logo"
 				width="300px"
 			/>
+			<NavLink href="/white-paper">View our Whitepaper</NavLink>
 		</Flex>
 	);
 };
