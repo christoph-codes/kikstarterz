f51e449c2ec0a64819cb01854877bde8815d571b
Christopher Jones <tkcwebdev@gmail.com>
2023-01-25 15:59:22 -0800

    main: Add link to main logo

--- web/components/Header/index.tsx
+++ web/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { Flex, Image } from "@chakra-ui/react";
+import Link from "next/link";
 import NavLink from "../NavLink";
 
 const Header = () => {
@@ -11,11 +12,13 @@ const Header = () => {
 			borderBottom="solid 1px"
 			borderBottomColor="brand.black.hover"
 		>
-			<Image
-				src="/kikstarterz_hori_logo_light.svg"
-				alt="Kikstarterz Logo"
-				width="300px"
-			/>
+			<Link href="/">
+				<Image
+					src="/kikstarterz_hori_logo_light.svg"
+					alt="Kikstarterz Logo"
+					width="300px"
+				/>
+			</Link>
 			<NavLink href="/white-paper">View our Whitepaper</NavLink>
 		</Flex>
 	);
