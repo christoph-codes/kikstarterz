a9737e99612711a6c0ee8e8cdca9472f3490b910
Christopher Jones <tkcwebdev@gmail.com>
2023-02-10 15:48:52 -0800

    12: Add api in front of all routes and make getting data dynamic

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import routes from './routes';
 const app = express();
 const port = 5555;
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
