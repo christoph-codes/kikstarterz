0e2cd11258ff57cda4ce4f4ccfbd2871b3258af1
Christopher Jones <tkcwebdev@gmail.com>
2023-02-19 22:44:59 -0800

    28: Create issue token function and rename routes to accomodate serving a new token when user is valid

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import admin from "../config/firebase";
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
 	try {
         if(req.headers.authtoken && typeof req.headers.authtoken === 'string') {
-            admin.auth().verifyIdToken(req.headers.authtoken)
+            admin.auth.verifyIdToken(req.headers.authtoken)
             .then(() => {
                 console.log('Got in!');
                 next();
@@ -20,3 +20,17 @@ export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
 		res.status(500).send({ status: 'Failed checking a Kikstarterz Auth' });
 	}
 };
+
+export const issueToken = (req: Request, res: Response) => {
+    try {
+        const {uid} = req.body.user;
+        admin.auth.createCustomToken(uid).then(token => {
+            res.status(200).send({ status: 'success', token});
+            // TODO: Issue token to headers for verifying
+        }).catch(err => {
+            res.status(400).send({ error: err});
+        })
+    } catch(err) { 
+        res.status(500).send({ error:  err})
+    }
+}
