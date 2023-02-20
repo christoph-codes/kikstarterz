326759a9bf61ea52d967b14825de0b1d89c2539e
Christopher Jones <54452490+christoph-codes@users.noreply.github.com>
2023-02-20 13:21:42 -0800

    Update firebase.ts
    Update location for render

--- api/src/config/firebase.ts
+++ api/src/config/firebase.ts
@@ -1,6 +1,6 @@
 import admin, { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount: ServiceAccount = require('../../firebase-service-account.json');
+const serviceAccount: ServiceAccount = require('../firebase-service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
