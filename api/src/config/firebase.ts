4ff42e7f5101d5c517f44d0341301d2bbdd890a8
Christopher Jones <tkcwebdev@gmail.com>
2023-02-20 20:34:39 -0800

    main-api: Update firebase config to not use json object import since Render doesnt support it

--- api/src/config/firebase.ts
+++ api/src/config/firebase.ts
@@ -1,8 +1,29 @@
 import admin, { ServiceAccount } from 'firebase-admin';
 
-const serviceAccount: ServiceAccount = require('../firebase-service-account.json');
+import dotenv from 'dotenv';
+dotenv.config();
+
+// @ts-ignore
+const { privateKey } = JSON.parse(process.env.FIRE_private_key);
+
+const serviceAccount: ServiceAccount = {
+  // @ts-ignore
+  "type": process.env.FIRE_type,
+  "project_id": process.env.FIRE_project_id,
+  "private_key_id": process.env.FIRE_private_key_id,
+  privateKey,
+  "client_email": process.env.FIRE_client_email,
+  "client_id": process.env.FIRE_client_id,
+  "auth_uri": process.env.FIRE_auth_uri,
+  "token_uri": process.env.FIRE_token_uri,
+  "auth_provider_x509_cert_url": process.env.FIRE_auth_provider_cert_url,
+  "client_x509_cert_url": process.env.FIRE_client_cert_url,
+};
+
+console.log('object', JSON.stringify(serviceAccount));
 
 admin.initializeApp({
+  // @ts-ignore
   credential: admin.credential.cert(serviceAccount)
 });
 
