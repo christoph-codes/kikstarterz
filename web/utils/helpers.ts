05692012c39a8d4b398b425f9a0713e5cb9b9240
christoph-codes <tkcwebdev@gmail.com>
2023-02-23 00:26:43 -0800

    main: Add helper functions for the api instance and fetcher for swr

---
+++ web/utils/helpers.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const kikapi = axios.create({
+	baseURL:
+		process.env.NODE_ENV === "development"
+			? "http://localhost:5555"
+			: "https://kikstarterz-api.onrender.com",
+	timeout: 1000,
+});
+
+export const fetcher = (url: string) => kikapi.get(url).then((res) => res.data);
