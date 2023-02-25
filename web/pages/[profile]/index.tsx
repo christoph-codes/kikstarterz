761a533a99a1eef139e57ac40bc873e939f33409
christoph-codes <tkcwebdev@gmail.com>
2023-02-24 19:13:31 -0800

    36: Use new file structure for the profile page

--- web/pages/[profile]/index.tsx
+++ web/pages/[profile]/index.tsx
@@ -5,23 +5,36 @@ import { fetcher } from "@/utils/helpers";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import Title from "@/components/Title";
 import DataPoint from "@/components/DataPoint";
-import Loader from "@/components/Loader";
+import styles from "./Profile.module.scss";
+import FourOhFour from "../404";
 
 const Profile = () => {
-	const { query } = useRouter();
+	const { query, push } = useRouter();
+
 	const { data, error, isLoading } = useSWR(
-		`/api/athletes/${query.profile}`,
-		fetcher
+		query.profile !== undefined ? `/api/athletes/${query.profile}` : null,
+		fetcher,
+		{
+			revalidateOnReconnect: false,
+			revalidateIfStale: false,
+		}
 	);
 
-	const user = data?.data;
+	if (isLoading || !data) {
+		return null;
+	}
 
-	if (error) return <div>failed to load</div>;
-	if (isLoading) return <Loader />;
+	if (error || data.error) {
+		return <FourOhFour />;
+		// push("/404");
+		// return;
+	}
 
+	const user = data.data;
 	return (
 		<PageTemplate>
 			<Box
+				className={styles.ProfileHero}
 				as="section"
 				p="8"
 				marginTop={4}
@@ -29,36 +42,68 @@ const Profile = () => {
 				borderRadius={16}
 				bgGradient="linear(to-tr, #0c191a, #2e2912)"
 			>
-				<Flex gap={8} flexDirection={{ base: "column", md: "row" }}>
+				<Flex
+					gap={{ base: 4, md: 8 }}
+					flexDirection={{ base: "column", md: "row" }}
+				>
+					<Box display={{ base: "block", md: "none" }}>
+						<Title h2 color="white" marginBottom="0">
+							{user.fname} {user.lname}
+						</Title>
+						{user.class && (
+							<Text
+								fontSize="xl"
+								fontWeight="bold"
+								fontStyle="italic"
+								textTransform="uppercase"
+								color="brand.primary.default"
+							>
+								Class of {user.class}
+							</Text>
+						)}
+					</Box>
+
 					<Image
 						width={{ base: "100%", md: 300 }}
 						borderRadius={16}
 						src="/christopher-jones-headshot.jpg"
 						objectFit="cover"
 					/>
-					<Box width="100%">
-						<Title h2 color="white" marginBottom="0">
-							{user.fname} {user.lname}
-						</Title>
-						<Text
-							fontSize="xl"
-							fontWeight="bold"
-							fontStyle="italic"
-							textTransform="uppercase"
-							color="brand.primary.default"
-						>
-							Class of 2023 {user.class}
-						</Text>
+					<Box width="100%" marginLeft={{ base: 0, md: 8 }}>
+						<Box display={{ base: "none", md: "block" }}>
+							<Title
+								display={{ base: "none", md: "block" }}
+								h2
+								color="white"
+								marginBottom="0"
+							>
+								{user.fname} {user.lname}
+							</Title>
+							{user.class && (
+								<Text
+									fontSize="xl"
+									fontWeight="bold"
+									fontStyle="italic"
+									textTransform="uppercase"
+									color="brand.primary.default"
+								>
+									Class of {user.class}
+								</Text>
+							)}
+						</Box>
 						<Flex
 							marginTop={4}
 							gap={8}
-							justifyContent="center"
 							flexDirection={{ base: "column", md: "row" }}
 						>
 							<Box
 								width={{ base: "100%", md: "50%" }}
+								maxWidth={300}
 								borderColor="brand.primary.default !important"
-								borderRight={{ base: "none", md: "2px solid" }}
+								borderRight={{
+									base: "none",
+									md: "2px solid",
+								}}
 							>
 								<DataPoint label="Hometown">
 									{user.location}
@@ -79,7 +124,10 @@ const Profile = () => {
 									</DataPoint>
 								)}
 							</Box>
-							<Box width="50%">
+							<Box
+								width={{ base: "100%", md: "50%" }}
+								maxWidth={300}
+							>
 								{(user.gpa || user.act || user.sat) && (
 									<DataPoint label="Academics">
 										{`GPA: ${user.gpa} | SAT: ${user.sat} | ACT: ${user.act}`}
