b830483d2a4d48e6aec90622aa4bc2ab22ddc1b5
christoph-codes <tkcwebdev@gmail.com>
2023-03-01 23:37:10 -0800

    22: Create card component

---
+++ web/components/Card/index.tsx
@@ -0,0 +1,23 @@
+import { Box, BoxProps } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+export interface ICard {
+	children?: ReactNode;
+}
+
+const Card = ({ children, ...styles }: ICard & BoxProps) => {
+	return (
+		<Box
+			marginBottom={{ base: 4, md: 8 }}
+			p={{ base: 4, md: 8 }}
+			bgGradient="linear(to-tr, #0c191a, #2e2912)"
+			borderRadius={16}
+			width="100%"
+			{...styles}
+		>
+			{children}
+		</Box>
+	);
+};
+
+export default Card;
